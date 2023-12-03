c240306c46b07e20b52ba793c17a14a98555803d
Jan Vondruška <100487718+Honza0010@users.noreply.github.com>
2023-12-03 22:10:11 +0100

    Probabilities

--- QDA.h
+++ QDA.h
@@ -12,11 +12,13 @@
 
 class QDA
 {
+	int size;
 	int k;	//Number of classes
 	int m;	//Dimension
 	std::vector<arma::mat> data;
 	std::vector<arma::mat> means;
 	std::vector<arma::mat> cov_matrices;
+	std::vector<double> probabs;
 
 public:
 	QDA(int k, int m, const std::string& filename);
@@ -30,13 +32,24 @@ private:
 
 	void calculate_cov_matrices();
 
+	void calculate_probabs();
+
 	void write()
 	{
 		std::cout << data[0] << std::endl;
+		/*std::cout << this->size << std::endl;
 
 		for (int i = 0; i < k; i++)
 		{
-			//std::cout << means[i] << std::endl;
+			for (int j = 0; j < m; j++)
+			{
+				std::cout << means[i](0,j) << " ";
+			}
+			std::cout << std::endl;
+		}*/
+
+		for (int i = 0; i < k; i++)
+		{
 			std::cout << cov_matrices[i] << std::endl;
 		}
 	}
@@ -49,12 +62,17 @@ QDA::QDA(int k, int m, const std::string& filename)
 	data = std::vector<arma::mat>(k, arma::mat(0, m));
 	means = std::vector<arma::mat>(k, arma::mat(1, m));
 	cov_matrices = std::vector<arma::mat>(k, arma::mat());
+	probabs = std::vector<double>(k, 0);
 
 	load_file(filename);
+
+	calculate_probabs();
 
 	calculate_means();
 
 	calculate_cov_matrices();
+
+	
 
 	write();
 }
@@ -64,13 +82,13 @@ QDA::QDA(int k, int m, const std::string& filename)
 void QDA::load_file(const std::string& filename)
 {
 	std::ifstream file;
-	file.open(filename);  
+	file.open(filename);
 	if (file.is_open())
 	{
+		int size_ = 0;
 		int x;
 		while (!file.eof())
 		{
-			
 			std::vector<double> help(m + 1, 0);
 			for (int i = 0; i <= this->m; i++)
 			{
@@ -85,16 +103,18 @@ void QDA::load_file(const std::string& filename)
 				}
 			}
 
-			int index = help[m]-1;
+			int index = help[m] - 1;
 
 			arma::mat& class_matrix = data[index];
 			class_matrix.resize(class_matrix.n_rows + 1, m);
 			for (int i = 0; i < m; i++)
 			{
-				class_matrix(class_matrix.n_rows-1, i) = help[i];
+				class_matrix(class_matrix.n_rows - 1, i) = help[i];
 			}
+			size_++;
 			//data[index].resize(data[index].n_rows + 1, m);
 		}
+		this->size = size_;
 		file.close();
 	}
 	else
@@ -119,6 +139,15 @@ void QDA::calculate_cov_matrices()
 	}
 }
 
+void QDA::calculate_probabs()
+{
+	for (int i = 0; i < k; i++)
+	{
+		probabs[i] = (double)data[i].n_rows / (double)this->size;
+		std::cout << probabs[i] << std::endl;
+	}
+}
+
 
 int QDA::predict_class(std::vector<double> input_data)
 {
@@ -127,7 +156,7 @@ int QDA::predict_class(std::vector<double> input_data)
 		throw std::logic_error("Bad dimension of input data");
 	}
 
-	arma::mat x(1,m);
+	arma::mat x(1, m);
 	for (int i = 0; i < this->m; i++)
 	{
 		x(0, i) = input_data[i];
@@ -140,9 +169,10 @@ int QDA::predict_class(std::vector<double> input_data)
 	for (int i = 0; i < k; i++)
 	{
 		//deltas[i] = arma::det(cov_matrices[i]) - (1.0/2.0) * (x-means[i])* arma::inv(cov_matrices[i])*arma::trans(x - means[i]);
-		deltas[i] =  0.0 - 1.0/2.0 * std::log(std::abs(arma::det(cov_matrices[i])));
-		arma::mat pom = (1.0/2.0) * (x - means[i]) * arma::inv(cov_matrices[i]) * arma::trans(x - means[i]);
-		deltas[i] -= pom(0,0);
+		deltas[i] = 0.0 - 1.0 / 2.0 * std::log(std::abs(arma::det(cov_matrices[i])));
+		arma::mat pom = (1.0 / 2.0) * (x - means[i]) * arma::inv(cov_matrices[i]) * arma::trans(x - means[i]);
+		deltas[i] -= pom(0, 0);
+		deltas[i] += std::log(probabs[i]);
 		std::cout << deltas[i] << std::endl;
 	}
 
@@ -155,10 +185,10 @@ int QDA::predict_class(std::vector<double> input_data)
 			index = i;
 			max = deltas[i];
 		}
-			
+
 	}
 
-	return index;
+	return (index+1);
 }
 
 
